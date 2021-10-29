dda5bb5603cdea66186f30805ee40ba0e568b1d2
oscar.ruiz <oscar.ruiz@kantox.com>
2021-10-29 18:20:38 +0200

    wip

--- examples/mfour/third/Main.java
+++ examples/mfour/third/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         int userAge = 0;
-        Boolean userStudies = false;
-        Boolean userWork = false;
+        String userStudies = "error";
+        String userWork = "error";
 
         // Enter username and press Enter
         System.out.println("Could you give me your age, please?");
@@ -20,34 +20,47 @@ public class Main {
 
         myObj.close();
 
-        if ((userAge >= 18 && userStudies) || userWork == false) {
+        if ((userAge >= 18 && userStudies.equals("yes")) || userWork.equals("no")) {
             System.out.println("Your beca has been approved.");
         } else {
             System.out.println("You dont fit on the requirements to get a beca.");
         }
     }
 
-    private static boolean handleUserResponseYesNo(Scanner scanner) {
-        String response = "";
+    private static String handleUserResponseYesNo(Scanner scanner) {
+        String response = "error";
         Boolean askAgain = true;
+
+        System.out.println("1cheeeker----------");
+        System.out.println(response);
+        System.out.println(askAgain);
+        System.out.println("----------");
 
         for (int i = 0; (i < 5) && askAgain; i++) {
             response = scanner.nextLine();
 
             if (responseIsValid(response) == true) {
                 askAgain = false;
+
+                System.out.println("2cheeeker----------");
+                System.out.println(response);
+                System.out.println(askAgain);
+                System.out.println("----------");
                 break;
+            } else {
+                System.out.println("3cheeeker----------");
+                System.out.println(response);
+                System.out.println(askAgain);
+                System.out.println("----------");
+
+                System.out.println("Invalid response. Must be yes / no.");
             }
         }
 
-        return responseIsSuccessfull(response);
+        return response;
     }
 
     private static boolean responseIsValid(String str) {
         return str.equals("yes") || str.equals("no");
-    }
-
-    private static boolean responseIsSuccessfull(String str) {
-        return str.equals("yes");
     }
 }
