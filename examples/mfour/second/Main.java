ded7b211b5c94d011afd87312ca84040c1cd8141
oscar.ruiz <oscar.ruiz@kantox.com>
2021-10-29 15:09:36 +0200

    m4 two

---
+++ examples/mfour/second/Main.java
@@ -0,0 +1,33 @@
+package examples.mfour.second;
+
+import java.util.Scanner; // import the Scanner class 
+
+class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int userAge = 0;
+
+        // Enter username and press Enter
+        System.out.println("Could you give me your age, please?");
+        userAge = myObj.nextInt();
+        myObj.close();
+
+        System.out.println(messageForAge(userAge));
+    }
+
+    private static String messageForAge(int age) {
+        String output = "";
+
+        if (age <= 5) {
+            output = "preescolar";
+        } else if (age <= 11) {
+            output = "primaria";
+        } else if (age <= 16) {
+            output = "ESO";
+        } else {
+            output = "FP o Universitat";
+        }
+
+        return output;
+    }
+}
