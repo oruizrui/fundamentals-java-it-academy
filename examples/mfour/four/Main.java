ee45c1bfa7f18791f5a96cbd7dff03db03c30ef5
oscar.ruiz <oscar.ruiz@kantox.com>
2021-10-29 21:01:37 +0200

    mfour four switch case

--- examples/mfour/four/Main.java
+++ examples/mfour/four/Main.java
@@ -19,40 +19,40 @@ public class Main {
     private static String switchCaseImpl(Integer month, String message) {
         switch (month) {
         case 1:
-            message = "January 31 days";
+            message = "January has 31 days";
             break;
         case 2:
-            message = "February 28 days";
+            message = "February has 28 days";
             break;
         case 3:
-            message = "March 31 days";
+            message = "March has 31 days";
             break;
         case 4:
-            message = "April 30 days";
+            message = "April has 30 days";
             break;
         case 5:
-            message = "May 31 days";
+            message = "May has 31 days";
             break;
         case 6:
-            message = "June 30 days";
+            message = "June has 30 days";
             break;
         case 7:
-            message = "July 31 days";
+            message = "July has 31 days";
             break;
         case 8:
-            message = "August 31 days";
+            message = "August has 31 days";
             break;
         case 9:
-            message = "September 30 days";
+            message = "September has 30 days";
             break;
         case 10:
-            message = "October 31 days";
+            message = "October has 31 days";
             break;
         case 11:
-            message = "November 30 days";
+            message = "November has 30 days";
             break;
         case 12:
-            message = "December 31 days";
+            message = "December has 31 days";
             break;
         }
 
