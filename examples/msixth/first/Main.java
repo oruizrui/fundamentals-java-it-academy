ad35405e26bec1d9996def8e32c70e54b55945ec
oscar.ruiz <oscar.ruiz@kantox.com>
2021-10-31 16:49:47 +0100

    wip

---
+++ examples/msixth/first/Main.java
@@ -0,0 +1,70 @@
+package examples.msixth.first;
+
+import java.util.Scanner; // import the Scanner class 
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+        Double firstNumber = 0.0;
+        Double secondNumber = 0.0;
+        Double mathResult = 0.0;
+        String operator = "";
+
+        // Enter username and press Enter
+        System.out.println("Could you give me a number?");
+        firstNumber = myScan.nextDouble();
+        System.out.println("Could you give me another number?");
+        secondNumber = myScan.nextDouble();
+        myScan.nextLine();
+        System.out.println("Could you give an operator? (+, -, *, / or %)");
+        operator = myScan.nextLine();
+        myScan.close();
+
+        if (operator == "/" && secondNumber == 0) {
+            System.out.println("I cant divide by 0");
+        } else {
+            switch (operator) {
+            case "+":
+                mathResult = addition(firstNumber, secondNumber);
+                break;
+            case "-":
+                mathResult = subtraction(firstNumber, secondNumber);
+                break;
+            case "*":
+                mathResult = multiplication(firstNumber, secondNumber);
+                break;
+            case "/":
+                mathResult = division(firstNumber, secondNumber);
+                break;
+            case "%":
+                mathResult = module(firstNumber, secondNumber);
+                break;
+            default:
+                System.out.println("no operator matched");
+            }
+            System.out.println(firstNumber + " " + operator + " " + secondNumber + ":");
+            System.out.println(mathResult);
+        }
+    }
+
+    private static Double addition(Double num1, Double num2) {
+        return num1 + num2;
+    }
+
+    private static Double subtraction(Double num1, Double num2) {
+        return num1 - num2;
+    }
+
+    private static Double multiplication(Double num1, Double num2) {
+        return num1 * num2;
+    }
+
+    private static Double division(Double num1, Double num2) {
+        return num1 / num2;
+    }
+
+    private static Double module(Double num1, Double num2) {
+        return num1 % num2;
+    }
+}
---
+++ examples/msixth/second/Main.java
@@ -0,0 +1,58 @@
+package examples.msixth.second;
+
+import java.util.Scanner; // import the Scanner class 
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+        Double ageAverage = 0.0;
+        int arrayLength = 0;
+
+        System.out.println("number of persons");
+        arrayLength = myScan.nextInt();
+        int[] ages = new int[arrayLength];
+
+        ages = requestAges(ages, myScan);
+        ageAverage = averageCalculator(ages);
+
+        System.out.println("Average age is: " + ageAverage);
+    }
+
+    private static int[] requestAges(int[] ages, Scanner scanner) {
+        for (int i = 0; (i < ages.length); i++) {
+            System.out.println("age of person" + " " + (i + 1));
+            ages[i] = responseValidator(scanner);
+        }
+
+        return ages;
+    }
+
+    private static Integer responseValidator(Scanner scanner) {
+        Integer response = -1;
+        Boolean askAgain = true;
+
+        for (int i = 0; (i < 5) && askAgain; i++) {
+            response = scanner.nextInt();
+
+            if (response >= 0 && response <= 120) {
+                askAgain = false;
+                break;
+            } else {
+                System.out.println("Invalid response. Age must be between 0 - 120.");
+            }
+        }
+
+        return response;
+    }
+
+    private static Double averageCalculator(int[] ages) {
+        int sumArray = 0;
+
+        for (int i = 0; i < ages.length; i++) {
+            sumArray = (sumArray + ages[i]);
+        }
+
+        return (double) sumArray / ages.length;
+    }
+}
