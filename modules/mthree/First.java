68dddf3ae69eb0f4eed0899946122bdfcfc6ad9c
oscar.ruiz <oscar.ruiz@kantox.com>
2021-11-02 17:08:54 +0100

    wip

--- modules/mthree/First.java
+++ modules/mthree/First.java
@@ -1,8 +1,7 @@
-package modules.mthree.first;
 
 import java.util.Scanner; // import the Scanner class 
 
-public class Main {
+public class First {
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in);
         String userName = "";
--- modules/mthree/Three.java
+++ modules/mthree/Three.java
@@ -1,8 +1,7 @@
-package modules.mthree.three;
 
 import java.util.Scanner; // import the Scanner class 
 
-public class Main {
+public class Three {
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in);
         int[] intArray = new int[3];
