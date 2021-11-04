7dcc5eab83b6020de79838c9079417a65e477e49
oscar.ruiz <oscar.ruiz@kantox.com>
2021-11-04 09:54:23 +0100

    wip

--- modules/mseventhdotfirst/Four.java
+++ modules/mseventhdotfirst/Four.java
@@ -1,4 +1,6 @@
 /* 
+(M7.1.) Exercici 4
+
 L’usuari ha d’introduir dos números, el programa retornarà “Un dels dos números és negatiu”, només si un dels dos números és negatiu.
 */
 
@@ -9,15 +11,23 @@ import java.util.Scanner; // import the Scanner class
 public class Four {
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in);
-        int monthAsString = 0;
-        String message = "Invalid day";
+        int firstNumber;
+        int secondNumber;
 
-        System.out.println("Could you give me a month, please? (1, 2, 3, etc.)");
-        monthAsString = myScan.nextInt();
+        System.out.println("Could you give me a number?");
+        firstNumber = myScan.nextInt();
+        System.out.println("Could you give me a another?");
+        secondNumber = myScan.nextInt();
         myScan.close();
 
-        // message = switchCaseImpl(monthAsString, message);
-        System.out.println(message);
-        System.out.println(monthAsString);
+        if (justOneNegative(firstNumber, secondNumber) || justOneNegative(secondNumber, firstNumber)) {
+            System.out.println("one of them is negative");
+        }
+
+        System.out.println("thanks!");
+    }
+
+    private static Boolean justOneNegative(int first, int second) {
+        return (first < 0 && second >= 0);
     }
 }
